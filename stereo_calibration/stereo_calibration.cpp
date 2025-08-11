71e81e735cb0410aa2ae5dee1069d6d37c0b047d
sewon.jeong <sw912d@gmail.com>
2025-08-11 22:49:41 +0900

    이미지 불러오기 구현

--- stereo_calibration/stereo_calibration.cpp
+++ stereo_calibration/stereo_calibration.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstdint>
+#include <opencv2/opencv.hpp>
 
 using std::uint64_t;
 
@@ -9,7 +10,7 @@ static uint64_t Solution(uint64_t A);
 
 int main()
 {
-	
+	Solution(0);
 	return 0;
 }
 //========//========//========//========//=======#//========//========//========//========//=======#
@@ -17,5 +18,18 @@ int main()
 
 uint64_t Solution(uint64_t A)
 {
+	std::string base_dir = "../../stereo_calibration/imgs/";
+	cv::Mat image = cv::imread(base_dir + "1.bmp");
+	if (image.empty()) 
+	{
+		std::cerr << "Error: Could not load the image!" << std::endl;
+		return -1;
+	}
+
+	// Display image
+	cv::imshow("Calibration Image", image);
+	cv::waitKey(0);
+	cv::destroyAllWindows();
+	
 	return 0;
 }
