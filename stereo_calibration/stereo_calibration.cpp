2a50b3a1476724ca32cdfdc615d1e2307c77df07
sewon.jeong <sw912d@gmail.com>
2025-08-12 23:19:32 +0900

    keypoint 대신 좌표 받는거로 수정

--- stereo_calibration/stereo_calibration.cpp
+++ stereo_calibration/stereo_calibration.cpp
@@ -7,8 +7,8 @@ using std::uint64_t;
 
 
 static uint64_t Solution(uint64_t A);
-std::vector<cv::KeyPoint> computeCircleCenterCoordinates(const cv::Mat& image);
-std::vector<cv::KeyPoint> findMostIsolatedPoints(const std::vector<cv::KeyPoint>& keyPoints, size_t K = 5);
+std::vector<cv::Point2f> computeCircleCenterCoordinates(const cv::Mat& image);
+std::vector<cv::Point2f> findMostIsolatedPointCoordinates(const std::vector<cv::Point2f>& keyPointCoordinates, size_t K = 5);
 
 int main()
 {
@@ -38,15 +38,20 @@ uint64_t Solution(uint64_t A)
 			continue; // Skip to the next file if loading fails
 		}
 
-		std::vector<cv::KeyPoint> keypoints = computeCircleCenterCoordinates(image); // Detect keypoints
-		auto selectedKeypoints = findMostIsolatedPoints(keypoints, 5); // Find the most isolated keypoints
+		std::vector<cv::Point2f> keyPointCoordinates = computeCircleCenterCoordinates(image); // Detect keypoints
+		auto selectedKeypointCoordinates = findMostIsolatedPointCoordinates(keyPointCoordinates, 5); // Find the most isolated keypoints
+
+		//sort the keypoints based on their coordinates
+		size_t W = image.size().width;
+		std::sort(selectedKeypointCoordinates.begin(), selectedKeypointCoordinates.end(), [&W](const cv::Point2f& a, const cv::Point2f& b) {
+			return a.x + W * a.y < b.x + W * b.y;
+			});
 
 		// Display the image with the furthest keypoints marked
-
 		cv::Mat outputImage = image.clone();
-		for (const auto& keypoint : selectedKeypoints) 
+		for (const auto& coordinate: selectedKeypointCoordinates)
 		{
-			cv::circle(outputImage, keypoint.pt, 5, cv::Scalar(0, 255, 0), -1); // Draw a circle around each keypoint
+			cv::circle(outputImage, coordinate, 5, cv::Scalar(0, 255, 0), -1); // Draw a circle around each keypoint
 		}
 		cv::imshow("Furthest Keypoints", outputImage); // Show the image with keypoints
 		cv::waitKey(0); // Wait for a key press to close the window
@@ -57,7 +62,7 @@ uint64_t Solution(uint64_t A)
 	return 0;
 }
 
-std::vector<cv::KeyPoint> computeCircleCenterCoordinates(const cv::Mat& image) 
+std::vector<cv::Point2f> computeCircleCenterCoordinates(const cv::Mat& image)
 {
 	// Blob Detector parameters
 	cv::SimpleBlobDetector::Params params;
@@ -83,28 +88,33 @@ std::vector<cv::KeyPoint> computeCircleCenterCoordinates(const cv::Mat& image)
 	// Detect keypoints
 	std::vector<cv::KeyPoint> keypoints;
 	detector->detect(image, keypoints);
+	std::vector<cv::Point2f> keypointCoords;
+	for (const auto& keypoint : keypoints)
+	{
+		keypointCoords.push_back(keypoint.pt);
+	}
 
-	return keypoints;
+	return keypointCoords;
 }
 
-std::vector<cv::KeyPoint> findMostIsolatedPoints(const std::vector<cv::KeyPoint>& keypoints, size_t K) 
+std::vector<cv::Point2f> findMostIsolatedPointCoordinates(const std::vector<cv::Point2f>& keyPointCoordinates, size_t K)
 {
 	// First, try searching with a loop. If it's slow, try a tree.
 	std::vector<double> closestNeighborDists;
 	std::vector<double> neighborDists;
-	closestNeighborDists.reserve(keypoints.size());
-	neighborDists.reserve(keypoints.size() - 1);
-	for (const auto& keypoint : keypoints)
+	closestNeighborDists.reserve(keyPointCoordinates.size());
+	neighborDists.reserve(keyPointCoordinates.size() - 1);
+	for (const auto& keypointCoord: keyPointCoordinates)
 	{
 		neighborDists.clear();
-		for (const auto& neighbor : keypoints)
+		for (const auto& neighborCoord : keyPointCoordinates)
 		{
-			if (neighbor.pt == keypoint.pt) // You can search by address, but for readability...
+			if (neighborCoord == keypointCoord) // You can search by address, but for readability...
 			{
 				continue;
 			}
 
-			neighborDists.push_back(cv::norm(neighbor.pt - keypoint.pt));
+			neighborDists.push_back(cv::norm(neighborCoord - keypointCoord));
 		}
 
 		closestNeighborDists.push_back(*std::min_element(neighborDists.begin(), neighborDists.end()));
@@ -119,16 +129,16 @@ std::vector<cv::KeyPoint> findMostIsolatedPoints(const std::vector<cv::KeyPoint>
 			return closestNeighborDists[a] > closestNeighborDists[b];
 		});
 
-	std::vector<cv::KeyPoint> outputKeypoints;
-	outputKeypoints.reserve(K);
+	std::vector<cv::Point2f> outputKeyPointCoords;
+	outputKeyPointCoords.reserve(K);
 
 
-	size_t topN = std::min(keypoints.size(), K);
+	size_t topN = std::min(keyPointCoordinates.size(), K);
 	for (int i = 0; i < topN; i++)
 	{
 		int id = idx[i];
-		outputKeypoints.push_back(keypoints[id]);
+		outputKeyPointCoords.push_back(keyPointCoordinates[id]);
 	}
 
-	return outputKeypoints; // Return the K most isolated keypoints
+	return outputKeyPointCoords; // Return the K most isolated keypoints
 }
